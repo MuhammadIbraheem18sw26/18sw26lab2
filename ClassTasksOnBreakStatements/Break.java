cd14656da0ba2ba5bcbc22a03bcac6697fa65739
MuhammadIbraheem18sw26 <49375508+MuhammadIbraheem18sw26@users.noreply.github.com>
2019-04-23 22:05:09 +0500

    Add files via upload

---
+++ ClassTasksOnBreakStatements/Break.java
@@ -0,0 +1,19 @@
+class Break{
+public static void main(String[] args)
+{
+// print even 
+for(int i=1;i<=20;i++)
+{
+for(int j=1;j<=5;j++)
+{	if(j==3)
+	{
+	break;	
+	}
+	System.out.print(j);
+}
+System.out.println("");
+}
+
+}
+
+}
---
+++ ClassTasksOnBreakStatements/Continue.java
@@ -0,0 +1,13 @@
+class Continue{
+public static void main(String[] args)
+{
+	// Even Numbers
+	System.out.println("Series of Even Numbers using Continue Statement");
+	for(int i=0;i<=20;i++){
+	{
+		if(i%2!=0)
+			continue;
+	}
+	System.out.println(i);}
+}
+}
